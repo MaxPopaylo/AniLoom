c98da9f6ab50ecc023e9d96fff1a5cbbf7f205a4
Maksym <max.popaylo@gmail.com>
2025-02-26 20:40:20 +0200

    feature/setup feature: setup start screen

--- app/src/main/java/ua/aniloom/presentation/pages/MainActivity.kt
+++ app/src/main/java/ua/aniloom/presentation/pages/MainActivity.kt
@@ -6,14 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import ua.aniloom.R
 import ua.aniloom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -23,6 +26,18 @@ class MainActivity : AppCompatActivity() {
             controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             controller.hide(WindowInsetsCompat.Type.navigationBars())
         }
+
+        setSupportActionBar(binding.mainToolbar)
+
+        val navHost = supportFragmentManager.findFragmentById(
+            R.id.mainFragments) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navHost.navController)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.searchPageFragment,
+        ))
+
+        setupActionBarWithNavController(navHost.navController, appBarConfiguration)
 
     }
 }
