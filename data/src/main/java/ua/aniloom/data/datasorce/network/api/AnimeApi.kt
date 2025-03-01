4786f865e2c158c59873303244699c24809a17ad
Maksym <max.popaylo@gmail.com>
2025-03-01 15:50:39 +0200

    feature/setup feature: add retrofit animeApi

--- data/src/main/java/ua/aniloom/data/datasorce/network/api/AnimeApi.kt
+++ data/src/main/java/ua/aniloom/data/datasorce/network/api/AnimeApi.kt
@@ -2,10 +2,15 @@ package ua.aniloom.data.datasorce.network.api
 
 import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.GET
-import retrofit2.http.Url
+import retrofit2.http.Query
 import ua.aniloom.data.datasorce.network.dto.PagingDto
+import ua.aniloom.data.datasorce.network.dto.anime.AnimePreviewDto
 
 interface AnimeApi {
-//    @GET
-//    suspend fun getAnimeAiringRanking(@Url url: String): ApiResponse<PagingDto>
+    @GET("anime/ranking")
+    suspend fun getAiringRankingAnime(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int,
+        @Query("fields") fields: String = AnimePreviewDto.MAL_FIELDS
+    ): ApiResponse<PagingDto<AnimePreviewDto>>
 }
