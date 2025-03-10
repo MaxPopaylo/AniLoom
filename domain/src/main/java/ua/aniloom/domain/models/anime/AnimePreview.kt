9d0c69182e3f8bad33d2b5011470e664dab165a4
Maksym <max.popaylo@gmail.com>
2025-03-10 13:13:07 +0200

    feature/setup feature: add preview interface for animePreview model

--- domain/src/main/java/ua/aniloom/domain/models/anime/AnimePreview.kt
+++ domain/src/main/java/ua/aniloom/domain/models/anime/AnimePreview.kt
@@ -1,15 +1,14 @@
 package ua.aniloom.domain.models.anime
 
 import ua.aniloom.domain.models.IBaseDiffModel
+import ua.aniloom.domain.models.PreviewModel
 
 data class AnimePreview(
     override val id: Int,
-    val title: String,
-    val mainPicture: String,
-
-    val mediaType: MediaType,
-    val aired: Aired,
-    val numEps: Int,
-
-    val score: Score
-): IBaseDiffModel<Int>
+    override val title: String,
+    override val mainPicture: String,
+    override val mediaType: MediaType,
+    override val aired: Aired,
+    override val numEps: Int,
+    override val score: Score
+): IBaseDiffModel<Int>, PreviewModel
